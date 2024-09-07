bca1e85aa6032e35f9f55a891dfadd995a109661
Yagamikira2007 <yagami.kira2024@gmail.com>
2024-09-07 21:08:52 +0630

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
 PICS = (environ.get('PICS', 'https://i.ibb.co/JxDdHJk/672eaef5d850.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/b69af2db776e4e85d21ec.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Open_Linkl")
-SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://i.ibb.co/JxDdHJk/672eaef5d850.jpg")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002170497151'))
